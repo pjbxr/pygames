9902d275c91f37a5c41005ba51245ed41a6a7fcb
pbj <prajaktabaral07@gmail.com>
2023-02-12 07:21:11 +0000

    we have now saved this file

--- rps.py
+++ rps.py
@@ -1,2 +1,38 @@
-if __name__ == '__main__':
+"""if __name__ == '__main__':
     print("welcome to rock paper scissor")
+"""
+from random import randint
+
+if __name__ == '__main__':
+    print(f'Hi, welcome to RPS!! Enter a valid Entry')
+    # create a list of play options
+    t = ["Rock", "Paper", "Scissors"]
+
+    # assign a random play to the computer
+    computer = t[randint(0, 2)]
+    # set player to false
+    player = False
+
+    while player == False:
+        # set player to true
+        player = input("Rock, Paper, Scissors?")
+
+        if player == computer:
+            print("Tie!")
+        elif player == "Rock":
+            if computer == "Paper!":
+                print("You lose!", computer, "covers", player)
+            else:
+                print("You win!", player, "smashes", computer)
+        elif player == "Paper":
+            if computer == "Scissors":
+                print("You lose!", computer, "cut", player)
+            else:
+                print("You win!", player, "covers", computer)
+        elif player == "Scissors":
+            if computer == "Rock":
+                print("You lose...", computer, "smashes", player)
+            else:
+                print("You win!", player, "cut", computer)
+        else:
+            print("That's not a valid play. Check your spelling!")
